8c83cc3a87f6625f6755e1d30a79fd4ff941631c
Hirdesh <hirdeshhkumar2005@gmail.com>
2025-07-28 23:29:00 +0530

    your message here

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ if (!fs.existsSync('./uploads')) {
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(() => console.log("✅ MongoDB connected"))
-  .catch((err) => console.error("❌ MongoDB connection error:", err));
+}).then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 // View engine setup
 app.set("view engine", "ejs");
